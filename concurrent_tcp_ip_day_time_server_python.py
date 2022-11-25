ea6447120db13fad82e773687b0e1f0dbce00aee
Siddhant <104164130+siddhant-shekhar@users.noreply.github.com>
2022-11-25 23:23:25 +0530

    Create concurrent_tcp_ip_day_time_server_python.py

---
+++ concurrent_tcp_ip_day_time_server_python.py
@@ -0,0 +1,51 @@
+# server.py 
+import socket                                         
+import time
+
+# create a socket object
+serversocket = socket.socket(
+	        socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = socket.gethostname()                           
+
+port = 9999                                           
+
+# bind to the port
+serversocket.bind((host, port))                                  
+
+# queue up to 5 requests
+serversocket.listen(5)                                           
+
+while True:
+    # establish a connection
+    clientsocket,addr = serversocket.accept()      
+
+    print("Got a connection from %s" % str(addr))
+    currentTime = time.ctime(time.time()) + "\r\n"
+    clientsocket.send(currentTime.encode('ascii'))
+    clientsocket.close()
+   
+  
+##########################################################################################################################################################################
+
+# client.py  
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = socket.gethostname()                           
+
+port = 9999
+
+# connection to hostname on the port.
+s.connect((host, port))                               
+
+# Receive no more than 1024 bytes
+tm = s.recv(1024)                                     
+
+s.close()
+
+print("The time got from the server is %s" % tm.decode('ascii'))
